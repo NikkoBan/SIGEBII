212b2d616dbb6ae97b20a11248dcceaf6e8217f2
YeremiSantiago <hernandezysr@gmail.com>
2025-06-12 02:20:18 -0400

    crear entidad SystemSettings en capa de dominio

---
+++ SIGEBI.Domain/Base/SystemSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGEBI.Domain.Base
+{
+    public class SystemSettings
+    {
+        public int SettingID { get; set; }
+        public string SettingName { get; set; } = string.Empty;
+        public string? SettingValue { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
