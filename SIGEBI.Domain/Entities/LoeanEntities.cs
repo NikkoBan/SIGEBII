b56ddecc4b6be09138b76dc819912b8194505db5
ICO-KAL <kalconcepcion031@gmail.com>
2025-06-12 19:21:24 -0400

    Cambios nuevos

---
+++ SIGEBI.Domain/Entities/LoeanEntities.cs
@@ -0,0 +1,14 @@
+﻿using SIGEBI.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGEBI.Domain.Entities
+{
+    public abstract class LoanEntity : BaseEntity <int>
+    {
+       public override int ID { get; set; }
+    }
+}
---
+++ SIGEBI.Domain/Entities/SystemSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGEBI.Domain.Entities
+{
+    public class SystemSetting
+    {
+        public int SettingId { get; set; }
+        public string SettingName { get; set; } = string.Empty;
+        public string SettingValue { get; set; } = string.Empty;
+        public string ValueDataType { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
