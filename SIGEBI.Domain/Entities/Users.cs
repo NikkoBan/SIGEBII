7aa1fff0e1334c723e588adba9f864a25597f31e
Diomar Arianny Fleming <ariannybrooks24@gmail.com>
2025-06-12 22:18:22 -0400

    Descripción de los cambios realizados

--- SIGEBI.Domain/Entities/Users.cs
+++ SIGEBI.Domain/Entities/Users.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SIGEBI.Domain.Entities
 {
-    internal class Users
+    
+
+    public class User
     {
+        public int UserId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string InstitutionalEmail { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string? InstitutionalIdentifier { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int RoleId { get; set; }
+        public bool IsActive { get; set; }
+        public Role? Role { get; set; }
+        public ICollection<UserLogin>? UserLogins { get; set; }
+        public ICollection<LoginHistory>? LoginHistories { get; set; }
+        public ICollection<Loan>? Loans { get; set; }
+        public ICollection<Reservation>? Reservations { get; set; }
+        public ICollection<Notification>? Notifications { get; set; }
+        public ICollection<Sanction>? Sanctions { get; set; }
+        public ICollection<SpecialMaterial>? RequestedSpecialMaterials { get; set; }
+        public ICollection<SpecialMaterial>? ApprovedSpecialMaterials { get; set; }
     }
 }
